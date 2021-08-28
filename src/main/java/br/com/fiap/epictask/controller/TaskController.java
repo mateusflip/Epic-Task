74d142b41701a14d92908495ebb225b2c63d35bb
Mateus Ramos <mateus.ramos1337@gmail.com>
2021-08-27 23:24:49 -0300

    Validação das informações inseridas no formulário

--- src/main/java/br/com/fiap/epictask/controller/TaskController.java
+++ src/main/java/br/com/fiap/epictask/controller/TaskController.java
@@ -1,11 +1,17 @@
 package br.com.fiap.epictask.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.epictask.model.Task;
 import br.com.fiap.epictask.repository.TaskRepository;
@@ -17,9 +23,12 @@ public class TaskController {
 	private TaskRepository repository;
 	
 	
-	@GetMapping(value="/task")
-	public String index() {
-		return "tasks";
+	@GetMapping("/task")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("tasks"); //Nome do arquivo html que quero retornar
+		List<Task> tasks = repository.findAll(); 
+		modelAndView.addObject("tasks", tasks);
+		return modelAndView;
 	}
 	
 	@RequestMapping("/task/new")
@@ -30,9 +39,12 @@ public class TaskController {
 	
 	
 	@PostMapping("/task")
-	public String save(Task task) {
+	public String save(@Valid Task task, BindingResult result) { //Valid irá implantar as regras de validação que setamos no Model
+		if(result.hasErrors()) {
+			return "task-form";
+		}
 		repository.save(task);
-		System.out.println("Salvando Tarefa" + task );
+		
 		return "tasks";
 	}
 }
--- src/main/java/br/com/fiap/epictask/model/Task.java
+++ src/main/java/br/com/fiap/epictask/model/Task.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.Getter;
@@ -17,8 +21,14 @@ public class Task {
 	//Importante estes nomes serem os mesmos da caixa de texto
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
 	private String title;
+	
+	@Size(min=10)
 	private String description;
+	
+	@Min(10)
 	private int points;
 
 	
